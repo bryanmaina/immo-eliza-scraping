dc2aa26b5920c54103659f116924c8a6a652a3da
Lalovan <anna.ivailova@gmail.com>
2025-11-06 14:25:04 +0100

    Zimmo scraper tweaks

--- src/scrapers/zimmo_scraper.py
+++ src/scrapers/zimmo_scraper.py
@@ -11,127 +11,149 @@ import re
 log = logging.getLogger(__name__)
 
 
-class ZimmoScrapper(BaseScraper):
-    def __init__(self, municipalities: list[str]):
+class ZimmoScraper(BaseScraper):
+    def __init__(self, municipalities: list[str]=[]):
         super().__init__("https://www.zimmo.be/nl/") #Calling BaseScraper.__init__
-        self.driver
         self.municipalities = municipalities 
         #municipalities = ["Antwerpen","Brugge","Brussel","Gent","Hasselt","Leuven","Aalst","Borgerhout","De Panne","Genk","Knokke-Heist","Mechelen","Oostende","Turnhout"]
     
-    def for_sale(self, municipalities): #Enter municipality and open search results page
-        input_box = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[placeholder='Zoek een gemeente']")))
-        input_box.clear()
-        input_box.send_keys(municipalities)
-        search_button = self.driver.find_element(By.CSS_SELECTOR, "button.search_button")
-        search_button.click()
+    def scrape(self, driver: WebDriver) -> list[PropertyData]:
+        driver.get(self.website_url)
+        WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.CSS_SELECTOR,'#didomi-notice-agree-button'))).click()
+        self.for_sale(driver,"Gent")
+        self.scrape_current_page(driver)
 
+    def for_sale(self, driver: WebDriver, municipalities): #Enter municipality and open search results page
+        driver.get(self.website_url)
+        
+        driver.implicitly_wait(5)
+        input_box = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[placeholder='Gemeente of postcode']")))
+        input_box.clear()
+        input_box.send_keys(f"{municipalities}\n")
+        search_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.search_button")))
+        driver.execute_script('arguments[0].click()', search_button)
+        
     
-    def collect_listing_urls(self): #For each listing, per each page, grab the href of the listing (no click yet)
+    def collect_listing_urls(self, driver:WebDriver): #For each listing, per each page, grab the href of the listing (no click yet)
         urls = []
         while True:
-            container = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".property-results")))
+            if len(urls) > 150:
+                break
+
+            container = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".property-results")))
             listings = container.find_elements(By.CSS_SELECTOR, ".property-item_title > a")
+           
             page_urls = [listing.get_attribute("href") for listing in listings]
             urls.extend(page_urls)
 
             try:
-                next_button = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "li.last a")))
-                self.driver.execute_script("arguments[0].click();", next_button)
-                WebDriverWait(self.driver, 5).until(EC.staleness_of(listings[0])) # Wait for page reload
-            except Exception:
+                driver.save_screenshot("next_button.png")
+
+                next_button = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "li.last > a")))
+                driver.execute_script("arguments[0].click();", next_button)
+                WebDriverWait(driver, 5).until(EC.staleness_of(listings[0])) # Wait for page reload
+                           
+            except Exception as e:
+                log.error(e)
                 break 
-            
-    def scrape_current_page(self): #By iteration, each url opens in a tab
-        urls = self.collect_listing_urls()
+        return urls
+      
+    def scrape_current_page(self, driver): #By iteration, each url opens in a tab
+        urls = self.collect_listing_urls(driver)
         all_data = []
         for url in urls:
-            data = self.extract_data_from_url(url)
+            data = self.extract_data_from_url(url, driver)
             all_data.append(data)
         return all_data
-
+   
     #Collects data from a single listing page
-    def extract_data_from_url(self, url):
-        self.driver.get(url)
-        property_id = self.driver.find_element(By.CSS_SELECTOR, ".title-row p").text
-        price = self.driver.find_element(By.CSS_SELECTOR, "div.price-box > div > span").text
-        type_of_sale = self.driver.find_element(By.CSS_SELECTOR, " ").text
+    def extract_data_from_url(self, url, driver):
+        driver.get(url)
+        try:
+            property_id = driver.find_element(By.CSS_SELECTOR, ".title-row p").text
+            price = driver.find_element(By.CSS_SELECTOR, "div.price-box > div > span").text
+            #type_of_sale = driver.find_element(By.CSS_SELECTOR, " ").text
+
         #property_type = self.driver.find_element(By.CSS_SELECTOR, "ul.main-features li:nth-child(3) .feature-value").text
         #number_of_rooms = self.driver.find_element(By.CSS_SELECTOR, "ul.main-features li:nth-child(5) .feature-value").text
         #living_area = self.driver.find_element(By.CSS_SELECTOR, "ul.main-features li:nth-child(4) .feature-value").text
         #garden_area = self.driver.find_element(By.CSS_SELECTOR, "#remainder > div.info-list > div > div.col-xs-5.info-value > i").text
         
         # Split locality and postcode via regex
-        full_address = self.driver.find_element(By.CSS_SELECTOR, "#main-features .section-title-block h2 span:first-child").text
-        match = re.search(r'(\d{4})\s+([A-Za-zÀ-ÿ\-]+)', full_address)
-        if match:
-            post_code = match.group(1)
-            locality_name = match.group(2)
+            full_address = driver.find_element(By.CSS_SELECTOR, "#main-features .section-title-block h2 span:first-child").text
+            match = re.search(r'(\d{4})\s+([A-Za-zÀ-ÿ\-]+)', full_address)
+            if match:
+                post_code = match.group(1)
+                locality_name = match.group(2)
         
-        #Searching for condition of building, type,living area, rooms, 
-        #checking the number of facades in Main features at the beginning of page
-        main_features = self.driver.find_elements(By.CSS_SELECTOR, ".main-features li")
-        data = {}
-        for li in main_features:
+            #Searching for condition of building, type,living area, rooms, 
+            #checking the number of facades in Main features at the beginning of page
+            main_features = driver.find_elements(By.CSS_SELECTOR, ".main-features li")
+            data = {}
+            for li in main_features:
+                try:
+                    label = li.find_element(By.CSS_SELECTOR, ".feature-label").text.strip().lower()
+                    value = li.find_element(By.CSS_SELECTOR, ".feature-value").text.strip().lower()
+                    data[label] = value #Creating a dictionary with parameters from this block
+                except:
+                    continue 
+                
+            state_of_building = data.get("Conditie", None)
+            property_type = data.get("Type", None)
+            living_area = data.get("Living area", None)
+            number_of_rooms = data.get("Slaapkamers", None)
+            
+            number_of_facade = data.get("bebouwing", None)
+
+            if number_of_facade == "gesloten":
+                number_of_facade = 1
+            elif number_of_facade == "open":
+                number_of_facade = 4
+            elif number_of_facade == "halfopen":
+                number_of_facade = 2
+
+
+            #Searching for key word ("terrace") in Layout block  
             try:
-                label = li.find_element(By.CSS_SELECTOR, ".feature-label").text.strip().lower()
-                value = li.find_element(By.CSS_SELECTOR, ".feature-value").text.strip().lower()
-                data[label] = value #Creating a dictionary with parameters from this block
-            except:
-                continue 
+                layout_items = driver.find_elements(By.CSS_SELECTOR, "#tab-detail .section-title-block")
+                layout_texts = [item.text.strip().lower() for item in layout_items]
+                terrace_area = "terras" in layout_texts or "terrace" in layout_texts  #Handles both Dutch and English
+
+            except Exception:
+                layout_texts = []
+                terrace_area = None
+
+            #Searching for other key words in Main description 
+            try:
+                overview_text = driver.find_element(By.CSS_SELECTOR,"#description > div > p").text.lower()
+                equipped_kitchen = "ingerichte keuken" in overview_text or "equipped kitchen" in overview_text
+                furnished = "gemeubeld" in overview_text or "furnished" in overview_text
+                open_fire = "haard" in overview_text or "fireplce" in overview_text
+                swimming_pool = "zwembad" in overview_text or "swimming pool" in overview_text
+                garden_area = "tuin" in overview_text or "garden" in overview_text
+                
+            except Exception:
+                overview_text = ""
+                equipped_kitchen = furnished = open_fire = None
+
+            return {
+                "property_id": property_id,
+                "locality_name": locality_name,
+                "post_code": post_code, 
+                "price": price,
+                "property_type": property_type, 
             
-        state_of_building = data.get("Conditie", None)
-        property_type = data.get("Type", None)
-        living_area = data.get("Living area", None)
-        number_of_rooms = data.get("Slaapkamers", None)
-        
-        number_of_facade = data.get("bebouwing", None)
-
-        if number_of_facade == "gesloten":
-            number_of_facade = 1
-        elif number_of_facade == "open":
-            number_of_facade = 4
-        elif number_of_facade == "halfopen":
-            number_of_facade = 2
-
-
-        #Searching for key word ("terrace") in Layout block  
-        try:
-            layout_items = self.driver.find_elements(By.CSS_SELECTOR, "#tab-detail .section-title-block")
-            layout_texts = [item.text.strip().lower() for item in layout_items]
-            terrace_area = "terras" in layout_texts or "terrace" in layout_texts  #Handles both Dutch and English
-
-        except Exception:
-            layout_texts = []
-            terrace_area = None
-
-        #Searching for other key words in Main description 
-        try:
-            overview_text = self.driver.find_element(By.CSS_SELECTOR,"#description > div > p").text.lower()
-            equipped_kitchen = "ingerichte keuken" in overview_text or "equipped kitchen" in overview_text
-            furnished = "gemeubeld" in overview_text or "furnished" in overview_text
-            open_fire = "haard" in overview_text or "fireplce" in overview_text
-            swimming_pool = "zwembad" in overview_text or "swimming pool" in overview_text
-            garden_area = "tuin" in overview_text or "garden" in overview_text
+                "number_of_rooms": number_of_rooms,
+                "living_area": living_area,
+                "equiped_kitchen": equipped_kitchen,
+                "furnished": furnished,
+                "open_fire": open_fire,
+                "terrace_area": terrace_area,
+                "garden_area": garden_area,
+                "number_of_facades": number_of_facade,
+                "swimming_pool": swimming_pool,
+                "state_of_building": state_of_building
+            }
+        except Exception as e:
+            log.info(e)
             
-        except Exception:
-            overview_text = ""
-            equipped_kitchen = furnished = open_fire = None
-
-        return {
-            "property_id": property_id,
-            "locality_name": locality_name,
-            "post_code": post_code, 
-            "price": price,
-            "property_type": property_type, 
-            "type_of_sale": type_of_sale,
-            "number_of_rooms": number_of_rooms,
-            "living_area": living_area,
-            "equiped_kitchen": equipped_kitchen,
-            "furnished": furnished,
-            "open_fire": open_fire,
-            "terrace_area": terrace_area,
-            "garden_area": garden_area,
-            "number_of_facades": number_of_facade,
-            "swimming_pool": swimming_pool,
-            "state_of_building": state_of_building
-        }
